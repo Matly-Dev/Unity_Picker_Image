812b6122ab3d22836ef1873bb235deba93a82142
Matly-Dev <matly@mango-byte.com>
2022-10-13 11:01:16 +0700

    Base code Example for Picker or select an image to a UI form

---
+++ Assets/Unimgpicker/Samples/PickerController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+
+namespace Kakera
+{
+    public class PickerController : MonoBehaviour
+    {
+        [SerializeField]
+        private Unimgpicker imagePicker;
+
+        [SerializeField]
+        private MeshRenderer imageRenderer;
+
+        [SerializeField]
+        private Image imageDisplay;
+
+        private int[] sizes = { 1024, 256, 16 };
+
+        void Awake()
+        {
+            imagePicker.Completed += (string path) =>
+            { StartCoroutine(LoadImage(path, imageRenderer)); };
+        }
+
+        public void OnPressShowPicker()
+        {
+            imagePicker.Show("Select Image", "unimgpicker");
+        }
+
+        private IEnumerator LoadImage(string path, MeshRenderer output)
+        {
+            var url = "file://" + path;
+            var unityWebRequestTexture = UnityWebRequestTexture.GetTexture(url);
+            yield return unityWebRequestTexture.SendWebRequest();
+
+            var texture = ((DownloadHandlerTexture)unityWebRequestTexture.downloadHandler).texture;
+            if (texture == null)
+            {
+                Debug.LogError("Failed to load texture url:" + url);
+            }
+
+            output.material.mainTexture = texture;
+
+            Sprite sprite = Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height), new Vector2(texture.width / 2, texture.height / 2));
+            imageDisplay.overrideSprite = sprite;
+        }
+    }
+}
